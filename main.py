93e3a86bf97784c3a39c9f9523fdf8f72c78d543
Jenish-Bhagiya <202301480@dau.ac.in>
2025-10-15 00:42:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def verify_password(plain_password, hashed_password):
 def get_password_hash(password):
     return pwd_context.hash(password)
 
-def create_access_token( dict, expires_delta: timedelta = None):
+def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
     expire = datetime.utcnow() + (expires_delta or timedelta(hours=24))
     to_encode.update({"exp": expire})
